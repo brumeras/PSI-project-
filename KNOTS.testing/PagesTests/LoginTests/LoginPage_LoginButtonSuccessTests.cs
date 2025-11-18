83b002c061074c44ba68371b8f3f6df0915a8079
Ugnė Jurkšaitytė <ugne.jurk@inbox.lt>
2025-11-18 21:11:40 +0200

    testai login.razor testavimui

---
+++ KNOTS.testing/PagesTests/LoginTests/LoginPage_LoginButtonSuccessTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using System;
+using Bunit;
+using KNOTS.Components.Pages;
+using NSubstitute;
+
+public class LoginPage_LoginButtonSuccessTests : LoginPageTestsBase
+{
+    [Fact]
+    public void Login_Button_Calls_UserService_And_Navigates_On_Success()
+    {
+        _userServiceMock.IsAuthenticated.Returns(false);
+        _userServiceMock.LoginUser(Arg.Any<string>(), Arg.Any<string>())
+            .Returns((true, "OK"));
+
+        var cut = Render<Login>();
+
+        cut.Find("#usernameInput").Change("john");
+        cut.Find("#passwordInput").Change("pass");
+
+        cut.Find("button.btn-login").Click();
+
+        _userServiceMock.Received(1).LoginUser("john", "pass");
+
+        var navigated = ((TestNavigationManager)_navManager).Uri;
+        Assert.Equal("/Home", new Uri(navigated).AbsolutePath);
+    }
+}
---
+++ KNOTS.testing/PagesTests/LoginTests/LoginTestBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components;
+using NSubstitute;
+using KNOTS.Services.Interfaces;
+using KNOTS.Components.Pages;
+using KNOTS.Services;
+
+public abstract class LoginPageTestsBase : TestContext
+{
+    protected readonly InterfaceUserService _userServiceMock;
+    protected readonly NavigationManager _navManager;
+
+    protected LoginPageTestsBase()
+    {
+        _userServiceMock = Substitute.For<InterfaceUserService>();
+        Services.AddSingleton(_userServiceMock);
+
+        Services.AddScoped<NavigationManager, TestNavigationManager>();
+        _navManager = Services.GetRequiredService<NavigationManager>();
+    }
+}
+
+public class TestNavigationManager : NavigationManager
+{
+    public TestNavigationManager()
+    {
+        Initialize("http://localhost/", "http://localhost/");
+    }
+
+    protected override void NavigateToCore(string uri, bool forceLoad)
+    {
+        Uri = ToAbsoluteUri(uri).ToString();
+    }
+}
