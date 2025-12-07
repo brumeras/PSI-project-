9ab883510c1ed4c14baf865f13e2faad273bafa3
Ugnė Jurkšaitytė <ugne.jurk@inbox.lt>
2025-12-07 19:22:59 +0200

    sutvarkytas failines testas

--- KNOTS.testing/ProgramTests/ServiceRegistrationTest.cs
+++ KNOTS.testing/ProgramTests/ServiceRegistrationTest.cs
@@ -1,20 +1,36 @@
 using KNOTS.Data;
+using KNOTS.Hubs;
 using KNOTS.Services;
 using KNOTS.Services.Chat;
 using KNOTS.Services.Compability;
 using KNOTS.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TestProject1.ProgramTests;
 
-public class ServiceRegistrationTest {
+public class ServiceRegistrationTest
+{
     [Fact]
-    public void Services_AreRegistered() {
+    public void Services_AreRegistered()
+    {
+        // Arrange
         var builder = WebApplication.CreateBuilder();
         var services = builder.Services;
-        services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TestDb"));
+
+        // Register EF Core InMemory
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseInMemoryDatabase("TestDb"));
+
+        // Register SignalR (required for IHubContext<T>)
+        services.AddSignalR();
+
+        // Register Hub itself (so DI can create it)
+        services.AddSingleton<ChatHub>();
+
+        // Register application services
         services.AddScoped<InterfaceLoggingService, LoggingService>(sp => new LoggingService("logs"));
         services.AddScoped<InterfaceSwipeRepository, SwipeRepository>();
         services.AddScoped<InterfaceCompatibilityCalculator, CompatibilityCalculator>();
@@ -22,7 +38,11 @@ public class ServiceRegistrationTest {
         services.AddScoped<InterfaceCompatibilityService, CompatibilityService>();
         services.AddSingleton<IGameRoomService, GameRoomService>();
         services.AddScoped<IMessageService, MessageService>();
+
+        // Act
         var provider = services.BuildServiceProvider();
+
+        // Assert
         Assert.NotNull(provider.GetService<InterfaceLoggingService>());
         Assert.NotNull(provider.GetService<InterfaceSwipeRepository>());
         Assert.NotNull(provider.GetService<InterfaceCompatibilityCalculator>());
@@ -30,5 +50,8 @@ public class ServiceRegistrationTest {
         Assert.NotNull(provider.GetService<InterfaceCompatibilityService>());
         Assert.NotNull(provider.GetService<IGameRoomService>());
         Assert.NotNull(provider.GetService<IMessageService>());
+        
+        var hubContext = provider.GetService<IHubContext<ChatHub>>();
+        Assert.NotNull(hubContext);
     }
 }
